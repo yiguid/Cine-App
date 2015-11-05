cc7da3b8318c523e1d62bc0f1898603df39627bf
Your Name Here <wt_yuanshengping@163.com>
2015-11-05 12:05:05 +0800

    my view

---
+++ cine/GuanZhuFirstTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  GuanZhuFirstTableViewCell.h
+//  cine
+//
+//  Created by Mac on 15/11/5.
+//  Copyright © 2015年 yiguid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GuanZhuFirstTableViewCell : UITableViewCell
+
+@end
---
+++ cine/GuanZhuFirstTableViewCell.m
@@ -0,0 +1,29 @@
+//
+//  GuanZhuFirstTableViewCell.m
+//  cine
+//
+//  Created by Mac on 15/11/5.
+//  Copyright © 2015年 yiguid. All rights reserved.
+//
+
+#import "GuanZhuFirstTableViewCell.h"
+
+@implementation GuanZhuFirstTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+    
+    CGFloat viewW = self.bounds.size.width;
+    CGFloat viewH = self.bounds.size.height;
+    
+    
+//    UIImageView *img = [[UIImageView alloc]initwi
+
+    // Configure the view for the selected state
+}
+
+@end
