07d6b7657bc3a1c115d2c34acfebd18d46156123
yiguid <yiguid@gmail.com>
2015-11-06 12:28:03 +0800

    movie

--- cine/MovieViewController.m
+++ cine/MovieViewController.m
@@ -12,8 +12,13 @@
 #import "AFNetworking.h"
 #import "MBProgressHUD.h"
 
+static const CGFloat ChoosePersonButtonHorizontalPadding = 80.f;
+static const CGFloat ChoosePersonButtonVerticalPadding = 20.f;
+
+
 @interface MovieViewController ()
 @property MBProgressHUD *hud;
+@property (nonatomic, strong) NSMutableArray *people;
 @end
 
 @implementation MovieViewController
@@ -26,8 +31,58 @@
     //hud.dimBackground = YES;//使背景成黑灰色，让MBProgressHUD成高亮显示
     self.hud.square = YES;//设置显示框的高度和宽度一样
     //    [self.hud show:YES];
+    
+    self.people = [[self defaultPeople] mutableCopy];
+    //左右滑动
+    self.frontCardView = [self popPersonViewWithFrame:[self frontCardViewFrame]];
+    [self.view addSubview:self.frontCardView];
+    
+    // Display the second ChoosePersonView in back. This view controller uses
+    // the MDCSwipeToChooseDelegate protocol methods to update the front and
+    // back views after each user swipe.
+    self.backCardView = [self popPersonViewWithFrame:[self backCardViewFrame]];
+    [self.view insertSubview:self.backCardView belowSubview:self.frontCardView];
+    
+    // Add buttons to programmatically swipe the view left or right.
+    // See the `nopeFrontCardView` and `likeFrontCardView` methods.
+    [self constructNopeButton];
+    [self constructLikedButton];
 }
 
+#pragma mark - MDCSwipeToChooseDelegate Protocol Methods
+
+// This is called when a user didn't fully swipe left or right.
+- (void)viewDidCancelSwipe:(UIView *)view {
+    NSLog(@"You couldn't decide on %@.", self.currentPerson.name);
+}
+
+// This is called then a user swipes the view fully left or right.
+- (void)view:(UIView *)view wasChosenWithDirection:(MDCSwipeDirection)direction {
+    // MDCSwipeToChooseView shows "NOPE" on swipes to the left,
+    // and "LIKED" on swipes to the right.
+    if (direction == MDCSwipeDirectionLeft) {
+        NSLog(@"You noped %@.", self.currentPerson.name);
+    } else {
+        NSLog(@"You liked %@.", self.currentPerson.name);
+    }
+    
+    // MDCSwipeToChooseView removes the view from the view hierarchy
+    // after it is swiped (this behavior can be customized via the
+    // MDCSwipeOptions class). Since the front card view is gone, we
+    // move the back card to the front, and create a new back card.
+    self.frontCardView = self.backCardView;
+    if ((self.backCardView = [self popPersonViewWithFrame:[self backCardViewFrame]])) {
+        // Fade the back card into view.
+        self.backCardView.alpha = 0.f;
+        [self.view insertSubview:self.backCardView belowSubview:self.frontCardView];
+        [UIView animateWithDuration:0.5
+                              delay:0.0
+                            options:UIViewAnimationOptionCurveEaseInOut
+                         animations:^{
+                             self.backCardView.alpha = 1.f;
+                         } completion:nil];
+    }
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -115,4 +170,136 @@
 - (IBAction)QQShare:(id)sender {
     [self share:@"qq"];
 }
+
+#pragma mark View Contruction
+
+- (CGRect)frontCardViewFrame {
+    CGFloat horizontalPadding = 20.f;
+    CGFloat topPadding = 60.f;
+    CGFloat bottomPadding = 200.f;
+    return CGRectMake(horizontalPadding,
+                      topPadding,
+                      CGRectGetWidth(self.view.frame) - (horizontalPadding * 2),
+                      CGRectGetHeight(self.view.frame) - bottomPadding);
+}
+
+- (CGRect)backCardViewFrame {
+    CGRect frontFrame = [self frontCardViewFrame];
+    return CGRectMake(frontFrame.origin.x,
+                      frontFrame.origin.y + 10.f,
+                      CGRectGetWidth(frontFrame),
+                      CGRectGetHeight(frontFrame));
+}
+
+// Create and add the "nope" button.
+- (void)constructNopeButton {
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    UIImage *image = [UIImage imageNamed:@"nope"];
+    button.frame = CGRectMake(ChoosePersonButtonHorizontalPadding,
+                              CGRectGetMaxY(self.backCardView.frame) + ChoosePersonButtonVerticalPadding,
+                              image.size.width,
+                              image.size.height);
+    [button setImage:image forState:UIControlStateNormal];
+    [button setTintColor:[UIColor colorWithRed:247.f/255.f
+                                         green:91.f/255.f
+                                          blue:37.f/255.f
+                                         alpha:1.f]];
+    [button addTarget:self
+               action:@selector(nopeFrontCardView)
+     forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
+}
+
+// Create and add the "like" button.
+- (void)constructLikedButton {
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    UIImage *image = [UIImage imageNamed:@"liked"];
+    button.frame = CGRectMake(CGRectGetMaxX(self.view.frame) - image.size.width - ChoosePersonButtonHorizontalPadding,
+                              CGRectGetMaxY(self.backCardView.frame) + ChoosePersonButtonVerticalPadding,
+                              image.size.width,
+                              image.size.height);
+    [button setImage:image forState:UIControlStateNormal];
+    [button setTintColor:[UIColor colorWithRed:29.f/255.f
+                                         green:245.f/255.f
+                                          blue:106.f/255.f
+                                         alpha:1.f]];
+    [button addTarget:self
+               action:@selector(likeFrontCardView)
+     forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
+}
+
+#pragma mark Control Events
+
+// Programmatically "nopes" the front card view.
+- (void)nopeFrontCardView {
+    [self.frontCardView mdc_swipe:MDCSwipeDirectionLeft];
+}
+
+// Programmatically "likes" the front card view.
+- (void)likeFrontCardView {
+    [self.frontCardView mdc_swipe:MDCSwipeDirectionRight];
+}
+
+- (NSArray *)defaultPeople {
+    // It would be trivial to download these from a web service
+    // as needed, but for the purposes of this sample app we'll
+    // simply store them in memory.
+    return @[
+             [[Person alloc] initWithName:@"Finn"
+                                    image:[UIImage imageNamed:@"finn"]
+                                      age:15
+                    numberOfSharedFriends:3
+                  numberOfSharedInterests:2
+                           numberOfPhotos:1],
+             [[Person alloc] initWithName:@"Jake"
+                                    image:[UIImage imageNamed:@"jake"]
+                                      age:28
+                    numberOfSharedFriends:2
+                  numberOfSharedInterests:6
+                           numberOfPhotos:8],
+             [[Person alloc] initWithName:@"Fiona"
+                                    image:[UIImage imageNamed:@"fiona"]
+                                      age:14
+                    numberOfSharedFriends:1
+                  numberOfSharedInterests:3
+                           numberOfPhotos:5],
+             [[Person alloc] initWithName:@"P. Gumball"
+                                    image:[UIImage imageNamed:@"prince"]
+                                      age:18
+                    numberOfSharedFriends:1
+                  numberOfSharedInterests:1
+                           numberOfPhotos:2],
+             ];
+}
+
+- (ChoosePersonView *)popPersonViewWithFrame:(CGRect)frame {
+    if ([self.people count] == 0) {
+        return nil;
+    }
+    
+    // UIView+MDCSwipeToChoose and MDCSwipeToChooseView are heavily customizable.
+    // Each take an "options" argument. Here, we specify the view controller as
+    // a delegate, and provide a custom callback that moves the back card view
+    // based on how far the user has panned the front card view.
+    MDCSwipeToChooseViewOptions *options = [MDCSwipeToChooseViewOptions new];
+    options.delegate = self;
+    options.threshold = 160.f;
+    options.onPan = ^(MDCPanState *state){
+        CGRect frame = [self backCardViewFrame];
+        self.backCardView.frame = CGRectMake(frame.origin.x,
+                                             frame.origin.y - (state.thresholdRatio * 10.f),
+                                             CGRectGetWidth(frame),
+                                             CGRectGetHeight(frame));
+    };
+    
+    // Create a personView with the top person in the people array, then pop
+    // that person off the stack.
+    ChoosePersonView *personView = [[ChoosePersonView alloc] initWithFrame:frame
+                                                                    person:self.people[0]
+                                                                   options:options];
+    [self.people removeObjectAtIndex:0];
+    return personView;
+}
+
 @end
