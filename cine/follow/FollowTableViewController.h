95cec19aac4191d02b4f3a4b8a27273fe8df8084
yiguid <yiguid@gmail.com>
2015-11-07 08:52:29 +0800

    follow

---
+++ cine/follow/FollowTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  FollowTableViewController.h
+//  cine
+//
+//  Created by Guyi on 15/11/7.
+//  Copyright © 2015年 yiguid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FollowTableViewController : UITableViewController
+- (IBAction)follow:(id)sender;
+- (IBAction)publish:(id)sender;
+
+@end
